353088331bf8475bcba115e044c9e7279c74f491
Jessica Paczuski <jz.paczuski@gmail.com>
2016-06-24 16:13:23 +0000

    Add es2015

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var App = React.createClass({
-  render: function () {
+class App extends React.Component{
+  render() {
     return (
       <div> Hello World </div>
     )
   }
-});
+};
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
